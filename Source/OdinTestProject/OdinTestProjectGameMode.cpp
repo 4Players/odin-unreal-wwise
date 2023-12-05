e4d97b172f3defe962f12d9ad449c7a52f8f7c63
Stephen <stephen.fuhrmann@outlook.de>
2023-12-05 11:54:59 +0100

    add odin wwise sample project

---
+++ Source/OdinTestProject/OdinTestProjectGameMode.cpp
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OdinTestProjectGameMode.h"
+#include "OdinTestProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AOdinTestProjectGameMode::AOdinTestProjectGameMode()
+{
+
+}
---
+++ Source/OdinTestProject/Public/AkOdinInputComponent.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AkAudioInputComponent.h"
+#include "AkOdinInputComponent.generated.h"
+
+
+class OdinMediaSoundGenerator;
+class UOdinPlaybackMedia;
+
+UCLASS(BlueprintType, Blueprintable, meta = (BlueprintSpawnableComponent))
+class ODINTESTPROJECT_API UAkOdinInputComponent : public UAkAudioInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAkOdinInputComponent(const class FObjectInitializer& ObjectInitializer);
+
+	virtual void DestroyComponent(bool bPromoteChildren) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Odin|Sound")
+	void AssignOdinMedia(UPARAM(ref) UOdinPlaybackMedia*& Media);
+
+	virtual void GetChannelConfig(AkAudioFormat& AudioFormat) override;
+	virtual bool FillSamplesBuffer(uint32 NumChannels, uint32 NumSamples, float** BufferToFill) override;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Odin|Sound")
+	UOdinPlaybackMedia* PlaybackMedia = nullptr;
+	TSharedPtr<OdinMediaSoundGenerator, ESPMode::ThreadSafe> SoundGenerator;
+
+	float* Buffer = nullptr;
+	int32 BufferSize = 0;
+};
