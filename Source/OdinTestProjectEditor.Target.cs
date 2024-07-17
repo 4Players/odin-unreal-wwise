7f7cd82f0a97ccd82a667678ccacf47b3edc549e
David Liebemann <david@scico.dev>
2024-07-17 19:33:54 +0200

    Updated for newer Odin Plugin Versions and ready to be upgraded to newer Unreal versions

--- Source/OdinTestProjectEditor.Target.cs
+++ Source/OdinTestProjectEditor.Target.cs
@@ -8,7 +8,7 @@ public class OdinTestProjectEditorTarget : TargetRules
 	public OdinTestProjectEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		ExtraModuleNames.Add("OdinTestProject");
 	}
 }
